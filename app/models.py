b0ae7e611fc6c711a7db78f066c4e938a540e63e
Zeynep Dal <erdalzeynep@gmail.com>
2018-07-19 13:00:17 +0200

    create first model

--- app/models.py
+++ app/models.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+
+class Tweet(models.Model):
+   author = models.CharField(max_length=150)
+   content = models.CharField(max_length=140)
+   date_and_time = models.DateTimeField(default=datetime.now)
+
+
+
