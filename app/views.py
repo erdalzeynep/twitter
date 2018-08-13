134cb2aef9ea4365a158c572fbfa6b518204f27c
Zeynep Dal <erdalzeynep@gmail.com>
2018-08-13 14:46:05 +0200

    Making some visual adjustment

--- app/views.py
+++ app/views.py
@@ -4,7 +4,6 @@ from __future__ import unicode_literals
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 
-from django.template import loader
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 
@@ -30,34 +29,41 @@ def list_tweets(request):
 
 
 def add_tweets(request):
-    if request.method == 'POST':
-        if request.POST.get('content') and request.POST.get('author'):
-            tweet = Tweet()
+    if request.user.is_authenticated():
+        if request.method == 'POST':
+            if request.POST.get('content'):
+                tweet = Tweet()
+                tweet.content = request.POST.get('content')
+                tweet.author = request.user.username
+                tweet.save()
+                return redirect('/list-tweets')
+        else:
+
+            return render(request, 'app/add-tweets.html')
+
+    else:
+        return redirect('/login-page')
+
+
+def update_tweet(request, tweet_id):
+    if request.user.is_authenticated():
+        if request.method == 'GET':
+            tweet = get_object_or_404(Tweet, pk=tweet_id)
+            return render(request, 'app/update-tweet.html', {'tweet': tweet})
+        else:
+            tweet = get_object_or_404(Tweet, pk=tweet_id)
             tweet.content = request.POST.get('content')
             tweet.author = request.POST.get('author')
             tweet.save()
             return redirect('/list-tweets')
     else:
-        return render(request, 'app/add-tweets.html')
-
-
-def update_tweet(request, tweet_id):
-    if request.method == 'GET':
-        tweet = get_object_or_404(Tweet, pk=tweet_id)
-        return render(request, 'app/update-tweet.html', {'tweet': tweet})
-    else:
-        tweet = get_object_or_404(Tweet, pk=tweet_id)
-        tweet.content = request.POST.get('content')
-        tweet.author = request.POST.get('author')
-        tweet.save()
-        return redirect('/list-tweets')
+        return redirect('/login-page')
 
 
 def delete_tweet(request, tweet_id):
     tweet = get_object_or_404(Tweet, pk=tweet_id)
     tweet.delete()
     return redirect('/list-tweets')
-
 
 
 def signup(request):
